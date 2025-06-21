77b91c00d0cf0b22a6ff1cc679cd3a34d4d842fb
Jesse Redl <jredl@vendasta.com>
2025-06-21 11:00:14 -0600

    fix the site config

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Local SEO',
+  tagline: 'Boost your clients\' local rankings with AI-powered software.',
   favicon: 'img/logo.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
