18e5bc5478e03d089888f9660ac0806a267e8135
Jesse Redl <jredl@vendasta.com>
2025-06-22 10:08:28 -0600

    fix links within local seo docs

--- docusaurus/docusaurus.config.ts
+++ docusaurus/docusaurus.config.ts
@@ -81,7 +81,7 @@ const config: Config = {
           items: [
             {
               label: 'Getting started',
-              to: '/docs/intro',
+              to: '/docs/overview',
             },
           ],
         },
